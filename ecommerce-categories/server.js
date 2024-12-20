5d260f80d2c625338ff1e3d8030417b87fbe3ced
Aghaz Junaid <aghaz.dev@gmail.com>
2024-12-20 16:37:33 +0530

    adding category ui

--- ecommerce-categories/server.js
+++ ecommerce-categories/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const categoryRoutes = require('./routes/categoryRoutes');
+const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-
+app.use(cors())
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/ecommerce');
 
---
+++ ecommerce-frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CategoryTree from './components/CategoryTree';
+
+function App() {
+  return (
+    <>
+      <CategoryTree />
+    </>
+  );
+}
+
+export default App;
